f1bafc787fec5e9ad9f7366aaff122676e55d3ee
raissareyes <00001082176965sp@al.educacao.sp.gov.br>
2024-05-21 08:02:40 -0300

    adicionando os arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,1 +1,44 @@
+Y < posicaoVertical) {
+    posicaoVertical--;
+}
+  
+if(mouseY > posicaoVertical) {
+    posicaoVertical++;
+}
+function draw() {
+  
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  
+  if(mouseX < posicaoHorizontal) {
+    posicaoHorizontal--;
+  }
+  
+  if(mouseX > posicaoHorizontal) {
+    posicaoHorizontal++;
+  }
+  
+  if(mouseY < posicaoVertical) {
+    posicaoVertical--;
+  }
+  
+  if(mouseY > posicaoVertical) {
+    posicaoVertical++;
+  }
+  
+}
+function setup() {
+  createCanvas(400, 400);
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+if(mouseIsPressed){
+    
+}
+if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+}
+
 
